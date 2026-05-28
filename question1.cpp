836725f953f18d53f2f8f2520ae772449c6bb51d
akashmishra530350 <akashmishra530350@gmail.com>
2026-05-28 10:22:55 +0530

    Add files via upload

--- question1.cpp
+++ question1.cpp
@@ -1,1 +1,16 @@
+//Write a program to Calculate sum of first N natural numbers.
+#include <iostream>
+using namespace std;
+int main(){
+    // enter any natural number.
+    int num,sum=0,i=1;
+    cout<<"enter any natural number: ";
+    cin>>num;
+    while(i<=num){
+        sum +=i;
+        i++;
+    }
+    cout<<sum;
+    return 0;
+}
 
